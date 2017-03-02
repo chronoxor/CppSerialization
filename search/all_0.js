598e7f5c0fc162c760498b56d4b628fd233628c9
Travis CI <travis@travis-ci.org>
2017-03-02 23:34:23 +0000

    Update generated documentation

--- search/all_0.js
+++ search/all_0.js
@@ -1,9 +1,7 @@
 var searchData=
 [
-  ['account',['Account',['../struct_my_domain_1_1_account.html',1,'MyDomain']]],
-  ['account',['Account',['../struct_my_domain_1_1_account.html#aec93cf9c9a3c4a546a12256645693123',1,'MyDomain::Account::Account()'],['../struct_my_domain_1_1_account.html#a541cd6e7ebc1585a0c8aa25e25fc9283',1,'MyDomain::Account::Account(int id, const char *name, const char *currency, double amount)']]],
-  ['accountbuilder',['AccountBuilder',['../struct_my_domain_1_1_flat_buffers_1_1_account_builder.html',1,'MyDomain::FlatBuffers']]],
-  ['accountbuilder',['AccountBuilder',['../struct_my_domain_1_1_flat_buffers_1_1_account_builder.html#a4538c9a5c033bb75111f824c03dcff96',1,'MyDomain::FlatBuffers::AccountBuilder']]],
+  ['account',['Account',['../struct_my_domain_1_1_account.html',1,'MyDomain::Account'],['../struct_my_domain_1_1_account.html#aec93cf9c9a3c4a546a12256645693123',1,'MyDomain::Account::Account()'],['../struct_my_domain_1_1_account.html#a541cd6e7ebc1585a0c8aa25e25fc9283',1,'MyDomain::Account::Account(int id, const char *name, const char *currency, double amount)']]],
+  ['accountbuilder',['AccountBuilder',['../struct_my_domain_1_1_flat_buffers_1_1_account_builder.html',1,'MyDomain::FlatBuffers::AccountBuilder'],['../struct_my_domain_1_1_flat_buffers_1_1_account_builder.html#a4538c9a5c033bb75111f824c03dcff96',1,'MyDomain::FlatBuffers::AccountBuilder::AccountBuilder()']]],
   ['add_5famount',['add_Amount',['../struct_my_domain_1_1_flat_buffers_1_1_balance_builder.html#ac1c144a5a012e08176c315934e64eacf',1,'MyDomain::FlatBuffers::BalanceBuilder']]],
   ['add_5fcurrency',['add_Currency',['../struct_my_domain_1_1_flat_buffers_1_1_balance_builder.html#a394f66264670a8669fe7db98a47d138d',1,'MyDomain::FlatBuffers::BalanceBuilder']]],
   ['add_5fid',['add_Id',['../struct_my_domain_1_1_flat_buffers_1_1_order_builder.html#a634373cf9cc71aec1965d3a331264807',1,'MyDomain::FlatBuffers::OrderBuilder::add_Id()'],['../struct_my_domain_1_1_flat_buffers_1_1_account_builder.html#a3526b3c3c00e18339d6e5bcad3ddd25b',1,'MyDomain::FlatBuffers::AccountBuilder::add_Id()']]],
@@ -16,5 +14,5 @@ var searchData=
   ['add_5fvolume',['add_Volume',['../struct_my_domain_1_1_flat_buffers_1_1_order_builder.html#a7a4847abac5efbcec6c4a185b1a475c3',1,'MyDomain::FlatBuffers::OrderBuilder']]],
   ['add_5fwallet',['add_Wallet',['../struct_my_domain_1_1_flat_buffers_1_1_account_builder.html#a9828401126e55bee3e16d9becbc3689d',1,'MyDomain::FlatBuffers::AccountBuilder']]],
   ['addorder',['AddOrder',['../struct_my_domain_1_1_account.html#aad8f3489c6e2c2277426933b8dfd7d14',1,'MyDomain::Account']]],
-  ['amount',['Amount',['../struct_my_domain_1_1_balance.html#ad4ee14cbe2f7a08c41a590e2895c7972',1,'MyDomain::Balance::Amount()'],['../struct_my_domain_1_1_flat_buffers_1_1_f_l_a_t_b_u_f_f_e_r_s___f_i_n_a_l___c_l_a_s_s.html#a313e161094a2f6fce08886a1fcd61755',1,'MyDomain::FlatBuffers::FLATBUFFERS_FINAL_CLASS::Amount()']]]
+  ['amount',['Amount',['../struct_my_domain_1_1_balance.html#ad4ee14cbe2f7a08c41a590e2895c7972',1,'MyDomain::Balance::Amount()'],['../struct_my_domain_1_1_flat_buffers_1_1_f_l_a_t_b_u_f_f_e_r_s___f_i_n_a_l___c_l_a_s_s.html#a29f25a080fa9697000b5594ab201d045',1,'MyDomain::FlatBuffers::FLATBUFFERS_FINAL_CLASS::Amount()']]]
 ];
