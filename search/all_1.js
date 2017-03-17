77362182e3a8dc8a4775d2aa8bbb169f3c2ded24
Travis CI <travis@travis-ci.org>
2017-03-17 20:23:59 +0000

    Update generated documentation

--- search/all_1.js
+++ search/all_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['deserializer',['Deserializer',['../class_cpp_serialization_1_1_j_s_o_n_1_1_deserializer.html',1,'CppSerialization::JSON::Deserializer'],['../class_cpp_serialization_1_1_j_s_o_n_1_1_deserializer.html#acae12b0aea3bf254cb12b67228c5dc0c',1,'CppSerialization::JSON::Deserializer::Deserializer()=delete'],['../class_cpp_serialization_1_1_j_s_o_n_1_1_deserializer.html#a74b3346d47844a812dd80333daaef58c',1,'CppSerialization::JSON::Deserializer::Deserializer(const Deserializer &amp;)=delete'],['../class_cpp_serialization_1_1_j_s_o_n_1_1_deserializer.html#a40ec8627c9006ed987c09cb426484994',1,'CppSerialization::JSON::Deserializer::Deserializer(Deserializer &amp;&amp;)=delete']]],
+  ['deserializer',['Deserializer',['../class_cpp_serialization_1_1_j_s_o_n_1_1_deserializer.html',1,'CppSerialization::JSON']]],
+  ['deserializer',['Deserializer',['../class_cpp_serialization_1_1_j_s_o_n_1_1_deserializer.html#acae12b0aea3bf254cb12b67228c5dc0c',1,'CppSerialization::JSON::Deserializer::Deserializer()=delete'],['../class_cpp_serialization_1_1_j_s_o_n_1_1_deserializer.html#a74b3346d47844a812dd80333daaef58c',1,'CppSerialization::JSON::Deserializer::Deserializer(const Deserializer &amp;)=delete'],['../class_cpp_serialization_1_1_j_s_o_n_1_1_deserializer.html#a40ec8627c9006ed987c09cb426484994',1,'CppSerialization::JSON::Deserializer::Deserializer(Deserializer &amp;&amp;)=delete']]],
   ['deserializer_2eh',['deserializer.h',['../deserializer_8h.html',1,'']]],
   ['deserializer_2einl',['deserializer.inl',['../deserializer_8inl.html',1,'']]]
 ];
