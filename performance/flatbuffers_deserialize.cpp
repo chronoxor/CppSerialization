025bc75a98242eb1e84ba3218fc5b0021dceaed3
Ivan Shynkarenka <chronoxor@gmail.com>
2018-02-23 21:19:49 +0300

    update

--- performance/flatbuffers_deserialize.cpp
+++ performance/flatbuffers_deserialize.cpp
@@ -14,12 +14,11 @@ class DeserializationFixture
 {
 protected:
     flatbuffers::FlatBufferBuilder builder;
-    Account deserialized;
+    Account account;
 
     DeserializationFixture()
     {
         // Create a new account with some orders
-        Account account(1, "Test", "USD", 1000);
         account.AddOrder(Order(1, "EURUSD", OrderSide::BUY, OrderType::MARKET, 1.23456, 1000));
         account.AddOrder(Order(2, "EURUSD", OrderSide::SELL, OrderType::LIMIT, 1.0, 100));
         account.AddOrder(Order(3, "EURUSD", OrderSide::BUY, OrderType::STOP, 1.5, 10));
@@ -32,7 +31,7 @@ protected:
 BENCHMARK_FIXTURE(DeserializationFixture, "FlatBuffers-Deserialize", iterations)
 {
     auto root = flat::GetAccount(builder.GetBufferPointer());
-    deserialized.DeserializeFlatBuffer(*root);
+    account.DeserializeFlatBuffer(*root);
 }
 
 BENCHMARK_MAIN()
--- performance/flatbuffers_serialize.cpp
+++ performance/flatbuffers_serialize.cpp
@@ -13,11 +13,12 @@ const uint64_t iterations = 1000000;
 class SerializationFixture
 {
 protected:
-    Account account;
     flatbuffers::FlatBufferBuilder builder;
+    Account account;
 
     SerializationFixture() : account(1, "Test", "USD", 1000)
     {
+        // Create a new account with some orders
         account.AddOrder(Order(1, "EURUSD", OrderSide::BUY, OrderType::MARKET, 1.23456, 1000));
         account.AddOrder(Order(2, "EURUSD", OrderSide::SELL, OrderType::LIMIT, 1.0, 100));
         account.AddOrder(Order(3, "EURUSD", OrderSide::BUY, OrderType::STOP, 1.5, 10));
