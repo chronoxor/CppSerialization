def4e6aa3560c3afc65627e4824583dd38887c9d
Travis CI <travis@travis-ci.org>
2020-03-09 14:10:58 +0000

    Update generated documentation

--- menudata.js
+++ menudata.js
@@ -1,25 +1,3 @@
-/*
-@licstart  The following is the entire license notice for the
-JavaScript code in this file.
-
-Copyright (C) 1997-2019 by Dimitri van Heesch
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of version 2 of the GNU General Public License as published by
-the Free Software Foundation
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-@licend  The above is the entire license notice
-for the JavaScript code in this file
-*/
 var menudata={children:[
 {text:"Main Page",url:"index.html"},
 {text:"Namespaces",url:"namespaces.html",children:[
@@ -41,7 +19,7 @@ var menudata={children:[
 {text:"p",url:"functions.html#index_p"},
 {text:"s",url:"functions.html#index_s"},
 {text:"v",url:"functions.html#index_v"},
-{text:"~",url:"functions.html#index__7E"}]},
+{text:"~",url:"functions.html#index_0x7e"}]},
 {text:"Functions",url:"functions_func.html",children:[
 {text:"d",url:"functions_func.html#index_d"},
 {text:"e",url:"functions_func.html#index_e"},
@@ -51,7 +29,7 @@ var menudata={children:[
 {text:"p",url:"functions_func.html#index_p"},
 {text:"s",url:"functions_func.html#index_s"},
 {text:"v",url:"functions_func.html#index_v"},
-{text:"~",url:"functions_func.html#index__7E"}]}]}]},
+{text:"~",url:"functions_func.html#index_0x7e"}]}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
--- search/files_3.js
+++ search/files_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['parser_2ecpp_49',['parser.cpp',['../parser_8cpp.html',1,'']]],
-  ['parser_2eh_50',['parser.h',['../parser_8h.html',1,'']]],
-  ['parser_2einl_51',['parser.inl',['../parser_8inl.html',1,'']]]
+  ['parser_2ecpp',['parser.cpp',['../parser_8cpp.html',1,'']]],
+  ['parser_2eh',['parser.h',['../parser_8h.html',1,'']]],
+  ['parser_2einl',['parser.inl',['../parser_8inl.html',1,'']]]
 ];
