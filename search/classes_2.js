0a812c8061cdb34eac5037dd90d8f871423903d0
Travis CI <travis@travis-ci.org>
2020-03-31 16:24:13 +0000

    Update generated documentation

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['serializationexception',['SerializationException',['../class_cpp_serialization_1_1_serialization_exception.html',1,'CppSerialization']]],
-  ['serializer',['Serializer',['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html',1,'CppSerialization::JSON']]]
+  ['serializationexception_41',['SerializationException',['../class_cpp_serialization_1_1_serialization_exception.html',1,'CppSerialization']]],
+  ['serializer_42',['Serializer',['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html',1,'CppSerialization::JSON']]]
 ];
