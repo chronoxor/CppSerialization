f0f1d5b445028cc440aa0a151a0c214d6603552f
Travis CI <travis@travis-ci.org>
2020-05-22 21:54:21 +0000

    Update generated documentation

--- search/all_9.js
+++ search/all_9.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['serializationexception_27',['SerializationException',['../class_cpp_serialization_1_1_serialization_exception.html',1,'CppSerialization']]],
-  ['serializer_28',['Serializer',['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html',1,'CppSerialization::JSON::Serializer&lt; OutputStream &gt;'],['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html#a427d39d057f8c232ec964136208469a5',1,'CppSerialization::JSON::Serializer::Serializer()=default'],['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html#aeb2cfe4d4cb07fc3b1a3db1b29f8bb5d',1,'CppSerialization::JSON::Serializer::Serializer(OutputStream &amp;stream)'],['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html#ad94a0fb6ecc3743689e599156ad0c1f7',1,'CppSerialization::JSON::Serializer::Serializer(const Serializer &amp;)=delete'],['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html#ad25f75a652fbc6192ccb6011a9b49074',1,'CppSerialization::JSON::Serializer::Serializer(Serializer &amp;&amp;)=default']]],
-  ['serializer_2eh_29',['serializer.h',['../serializer_8h.html',1,'']]],
-  ['serializer_2einl_30',['serializer.inl',['../serializer_8inl.html',1,'']]],
-  ['startarray_31',['StartArray',['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html#a1537d97befb95733a8f1bf0563a33e3d',1,'CppSerialization::JSON::Serializer']]],
-  ['startobject_32',['StartObject',['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html#aaa4ed835924e1cc4a7deab21af15d147',1,'CppSerialization::JSON::Serializer']]]
+  ['serializationexception',['SerializationException',['../class_cpp_serialization_1_1_serialization_exception.html',1,'CppSerialization']]],
+  ['serializer',['Serializer',['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html',1,'CppSerialization::JSON::Serializer&lt; OutputStream &gt;'],['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html#a427d39d057f8c232ec964136208469a5',1,'CppSerialization::JSON::Serializer::Serializer()=default'],['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html#aeb2cfe4d4cb07fc3b1a3db1b29f8bb5d',1,'CppSerialization::JSON::Serializer::Serializer(OutputStream &amp;stream)'],['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html#ad94a0fb6ecc3743689e599156ad0c1f7',1,'CppSerialization::JSON::Serializer::Serializer(const Serializer &amp;)=delete'],['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html#ad25f75a652fbc6192ccb6011a9b49074',1,'CppSerialization::JSON::Serializer::Serializer(Serializer &amp;&amp;)=default']]],
+  ['serializer_2eh',['serializer.h',['../serializer_8h.html',1,'']]],
+  ['serializer_2einl',['serializer.inl',['../serializer_8inl.html',1,'']]],
+  ['startarray',['StartArray',['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html#a1537d97befb95733a8f1bf0563a33e3d',1,'CppSerialization::JSON::Serializer']]],
+  ['startobject',['StartObject',['../class_cpp_serialization_1_1_j_s_o_n_1_1_serializer.html#aaa4ed835924e1cc4a7deab21af15d147',1,'CppSerialization::JSON::Serializer']]]
 ];
