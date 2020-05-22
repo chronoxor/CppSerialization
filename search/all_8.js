5396ac952abb54946511c44e5849366aa28692cc
Travis CI <travis@travis-ci.org>
2020-05-22 22:02:44 +0000

    Update generated documentation

--- search/all_8.js
+++ search/all_8.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['rapidjson_5fhas_5fstdstring',['RAPIDJSON_HAS_STDSTRING',['../json_8h.html#a2f2eef0ee4477f3fe5874703a66e997f',1,'json.h']]],
-  ['rapidjson_5fnamespace',['RAPIDJSON_NAMESPACE',['../json_8h.html#a743a79d3af927391fe3eb5c979136899',1,'json.h']]],
-  ['rapidjson_5fnamespace_5fbegin',['RAPIDJSON_NAMESPACE_BEGIN',['../json_8h.html#ad3806c8251fdc7da9618b7e922674ffc',1,'json.h']]],
-  ['rapidjson_5fnamespace_5fend',['RAPIDJSON_NAMESPACE_END',['../json_8h.html#af18f052a98b9f5df5448d39484b743c1',1,'json.h']]]
+  ['rapidjson_5fhas_5fstdstring_23',['RAPIDJSON_HAS_STDSTRING',['../json_8h.html#a2f2eef0ee4477f3fe5874703a66e997f',1,'json.h']]],
+  ['rapidjson_5fnamespace_24',['RAPIDJSON_NAMESPACE',['../json_8h.html#a743a79d3af927391fe3eb5c979136899',1,'json.h']]],
+  ['rapidjson_5fnamespace_5fbegin_25',['RAPIDJSON_NAMESPACE_BEGIN',['../json_8h.html#ad3806c8251fdc7da9618b7e922674ffc',1,'json.h']]],
+  ['rapidjson_5fnamespace_5fend_26',['RAPIDJSON_NAMESPACE_END',['../json_8h.html#af18f052a98b9f5df5448d39484b743c1',1,'json.h']]]
 ];
