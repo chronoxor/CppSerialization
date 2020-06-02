b4aa8f849ce940eb550e4f6ebba5bfbdb842bd12
Travis CI <travis@travis-ci.org>
2020-06-02 22:00:47 +0000

    Update generated documentation

--- search/files_4.js
+++ search/files_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['serializer_2eh',['serializer.h',['../serializer_8h.html',1,'']]],
-  ['serializer_2einl',['serializer.inl',['../serializer_8inl.html',1,'']]]
+  ['serializer_2eh_52',['serializer.h',['../serializer_8h.html',1,'']]],
+  ['serializer_2einl_53',['serializer.inl',['../serializer_8inl.html',1,'']]]
 ];
