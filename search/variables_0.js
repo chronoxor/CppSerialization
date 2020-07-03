303c449989725bfe51fc539583de85c97297cf00
Travis CI <travis@travis-ci.org>
2020-07-03 21:40:33 +0000

    Update generated documentation

--- search/variables_0.js
+++ search/variables_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['version_75',['version',['../namespace_cpp_serialization.html#abc3d6af18e6a0cfd26bccf986376aac6',1,'CppSerialization']]]
+  ['version',['version',['../namespace_cpp_serialization.html#abc3d6af18e6a0cfd26bccf986376aac6',1,'CppSerialization']]]
 ];
