36a1d86827d2f6329c8c2e6cc94002737085d593
Ivan Shynkarenka <chronoxor@gmail.com>
2020-09-10 04:06:11 +0300

    Update FBE protocol

--- proto/fbe/trade_final_models.cpp
+++ proto/fbe/trade_final_models.cpp
@@ -3,16 +3,6 @@
 // Source: trade.fbe
 // Version: 1.4.0.0
 
-#if defined(__GNUC__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-variable"
-#elif defined(_MSC_VER)
-#pragma warning(push)
-#pragma warning(disable:4065) // C4065: switch statement contains 'default' but no 'case' labels
-#pragma warning(disable:4100) // C4100: 'identifier' : unreferenced formal parameter
-#pragma warning(disable:4702) // C4702: unreachable code
-#endif
-
 #include "trade_final_models.h"
 
 namespace FBE {
@@ -570,9 +560,3 @@ size_t AccountFinalModel::deserialize(::trade::Account& value) const noexcept
 } // namespace trade
 
 } // namespace FBE
-
-#if defined(__GNUC__)
-#pragma GCC diagnostic pop
-#elif defined(_MSC_VER)
-#pragma warning(pop)
-#endif
